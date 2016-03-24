16fa6a083781af58b1da82446c01d1a0064edd3f
rotsee <leo_wallentin@hotmail.com>
2016-03-24 15:07:36 +0100

    make demo faster

--- demo.py
+++ demo.py
@@ -2,8 +2,8 @@ from modules.ted import Ted
 
 ted = Ted('test.csv')
 
-ted.apply_filter('country', 'DE')
 ted.apply_filter('security')
+ted.apply_filter('country', 'DE')
 
 companies = ted.get_companies()
 print companies
